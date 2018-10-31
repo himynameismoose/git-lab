6bb386528df10b50b666259b8ecec26e9499a28c
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2018-10-30 23:10:51 -0400

    Updated route

--- index.php
+++ index.php
@@ -13,8 +13,10 @@ require_once ('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Define a default route
-$f3->route('GET /', function () {
-    echo'<h1> Git Lab </h1>';
+$f3->route('GET /page1',
+    function () {
+        $view = new View;
+        echo $view->render('/views/page1.html');
 });
 
 //Run fat free
